6fa0163aee266233210bfe5d84588cf349abe136
Unto Ahti <developer.hekinav@gmail.com>
2025-12-18 20:01:05 +0200

    url quick fix

--- Web Installer/sim.js
+++ Web Installer/sim.js
@@ -25,7 +25,7 @@ let mapData = {
 
 let mode = OPTIONS.modes[0]
 let updates = 0
-const prodUrls = ["https://hekinav.github.io", "ltm.hekinav.dev", "hekinav.dev", "hki-ltm.pages.dev"]
+const prodUrls = ["hekinav.github.io", "ltm.hekinav.dev", "hekinav.dev", "hki-ltm.pages.dev"]
 const isProd = prodUrls.find(url => window.location.host.includes(url))
 
 Promise.all([fetch("./data/ledsInOrder.json"), loadSvg()]).then(([data, _]) => {
