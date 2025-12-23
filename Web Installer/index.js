f47ebba4f06b735890e746d5e67d077dcb60cf06
Unto Ahti <developer.hekinav@gmail.com>
2025-12-23 16:54:13 +0200

    tried to put a 3d model of the pcb on tje website

---
+++ Web Installer/index.js
@@ -0,0 +1,79 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x111111);
+
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 1, 5);
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+controls.target.set(0, 0, 0);
+controls.update();
+
+scene.add(new THREE.GridHelper(10, 10));
+scene.add(new THREE.AxesHelper(2));
+
+scene.add(new THREE.AmbientLight(0xffffff, 0.4));
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(5, 10, 5);
+scene.add(dirLight);
+
+const loader = new GLTFLoader();
+loader.load(
+  './data/pcb_cu.glb', 
+  (gltf) => {
+    const model = gltf.scene;
+
+    model.scale.set(100,100,100)
+
+
+    const box = new THREE.Box3().setFromObject(model);
+
+    const center = box.getCenter(new THREE.Vector3());
+
+    model.position.sub(center);
+
+
+    scene.add(model);
+
+    const outline = new THREE.BoxHelper(model, 0xffff00);
+    scene.add(outline);
+  },
+  undefined,
+  (error) => {
+    console.error('GLTF load error:', error);
+  }
+);
+
+// Resize
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Animate
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
